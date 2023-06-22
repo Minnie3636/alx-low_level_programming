b4f3f8de86300d1c5c3c5a7c57224a57c2486ec4
Minnie3636 <moyanaminenhle@gmail.com>
2023-06-21 20:03:17 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prog entry point
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
+	 
