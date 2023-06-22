3a75b842dc60bde4488faa90b546de9d6aaa76ce
Minnie3636 <moyanaminenhle@gmail.com>
2023-06-21 19:51:24 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prog entry point
+ * Return: always (0)
+ */
+int main(void)
+{
+	int c;
+	 
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c +'0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
